75b51d9a096d68be98c487177569c5dbf695dd54
helllomind <helllomind@hellomindmac.local>
2024-07-25 18:28:35 +0300

    ex5, correction ex4

--- src/main/java/ru/dobrocraft/ex5/Main.java
+++ src/main/java/ru/dobrocraft/ex5/Main.java
@@ -1,12 +1,16 @@
-package ru.dobrocraft.ex4;
+package ru.dobrocraft.ex5;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Map;
 
 public class Main {
 
     public HashMap<Integer, Integer> memoPaths = new HashMap<>();
     // храним максимальный путь для каждого потока чтобы не производить повторные вычисления
     public HashMap<Integer, int[]> threads = new HashMap<>();
+//    public HashMap<Integer, ArrayList<Integer>> distanceMap = new HashMap<>();
+    public int maxDistance = 0;
 
     public void readInput() {
         Scanner scanner = new Scanner(System.in);
@@ -32,28 +36,60 @@ public class Main {
                 maxPathsLen = Math.max(maxPathsLen, memoPaths.get(i + 1));
             }
         }
+        this.maxDistance = maxPathsLen;
         return maxPathsLen;
     }
 
     public int findMaxPath(int threadNumber, int currentWay) {
 
         if (threads.get(threadNumber).length == 0) {
+            memoPaths.put(threadNumber, currentWay);
             return currentWay;
         }
         int max_way = currentWay;
         for (int dependency : threads.get(threadNumber)) {
             if (!memoPaths.containsKey(dependency)) {
                 memoPaths.put(dependency, findMaxPath(dependency, 1));
+//                distanceMap.get(currentWay).add(dependency);
             }
             max_way = Math.max(max_way, memoPaths.get(dependency) + currentWay);
         }
         return max_way;
     }
 
+    public ArrayList<ArrayList<Integer>> calcSetsOfPaths() {
+        ArrayList<ArrayList<Integer>> setsOfPaths = new ArrayList<>();
+        for (int i = 0; i <= this.maxDistance; i++) {
+            ArrayList<Integer> setPaths = new ArrayList<>();
+            for (Map.Entry<Integer, Integer> entry : this.memoPaths.entrySet()) {
+                if (entry.getValue() == i) {
+                    setPaths.add(entry.getKey());
+                }
+            }
+            if (setPaths.isEmpty()) {
+                continue;
+            }
+            setsOfPaths.add(setPaths);
+        }
+        return setsOfPaths;
+    }
+
 
     public static void main(String[] args){
         Main main = new Main();
         main.readInput();
-        System.out.println(main.calcMaxPath());
+        main.calcMaxPath();
+        ArrayList<ArrayList<Integer>> result = main.calcSetsOfPaths();
+        System.out.println(result.size());
+        for (ArrayList<Integer> set : result) {
+            if (set.isEmpty()) {
+                continue;
+            }
+            System.out.print(set.size() + " ");
+            for (Integer i : set) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
         }
+    }
 }
